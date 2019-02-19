4e3bce39f0183bc0f93ccd70595c0c58336dc388
mike.kulasinski <mike.kulasinski@gmail.com>
2019-02-19 20:49:47 +0100

    overloading example

--- src/com/mike/kulasinski/Main.java
+++ src/com/mike/kulasinski/Main.java
@@ -3,6 +3,15 @@ package com.mike.kulasinski;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println(overloadingOne());
+        System.out.println(overloadingOne(10));
+    }
+
+    private static int overloadingOne() {
+        return 0;
+    }
+
+    private static String overloadingOne(int x) {
+        return "Test: " + x;
     }
 }
